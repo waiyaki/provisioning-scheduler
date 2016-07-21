aca5972bbd6619460b93c5a7909042f9a30601b4
James Muturi <muturijames3@gmail.com>
2016-07-21 22:35:42 +0300

    Initialize project

---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+const initialState = { name: 'Provisioning Scheduler' };
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
