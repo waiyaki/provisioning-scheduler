9673596b7c313b8544c51f0053131008909de6dc
James Muturi <muturijames3@gmail.com>
2016-08-13 17:26:13 +0300

    Make `acceptableDomains` configurable without editing source code.
    A list of available domains is now being read in from the env and
    added to the default list.

--- server/middleware/requireEmailWithDomains.js
+++ server/middleware/requireEmailWithDomains.js
@@ -1,3 +1,11 @@
+let acceptableDomainsFromEnv = process.env.ACCEPTABLE_DOMAINS;
+
+if (acceptableDomainsFromEnv) {
+  acceptableDomainsFromEnv = acceptableDomainsFromEnv.split(',').filter(
+    domain => !!domain // Remove any empty items from the list.
+  );
+}
+
 module.exports = (req, res, next, {
   /**
    * @param acceptableDomains
@@ -9,14 +17,22 @@ module.exports = (req, res, next, {
    * If you use this middleware and add other acceptableDomains, make sure to
    * add 'safaricom.co.ke' in your list as well. It's added here as a default
    * and will be overridden if other values are passed.
+   * It's HIGHLY recommended that you don't edit the code to add domains
+   * into the list, rather, add them into your environment as a comma
+   * separated list in an env variable named ACCEPTABLE_DOMAINS.
    *
    * Not going to validate that you didn't override 'safaricom.co.ke', I trust
    * that you're a developer and you what you're doing. ¯\_(ツ)_/¯
    */
   acceptableDomains = ['safaricom.co.ke']
 } = {}) => {
+  let domains = acceptableDomains;
+
+  if (acceptableDomainsFromEnv) {
+    domains = domains.concat(acceptableDomainsFromEnv);
+  }
   if (req.body.email) { // Only try to validate if we have an email.
-    for (const domain of acceptableDomains) {
+    for (const domain of domains) {
       if (new RegExp(`${domain}$`, 'i').test(req.body.email)) {
         return next();
       }
