95091a93d7b6c27010993edec82e02f6ee783b4f
James Muturi <muturijames3@gmail.com>
2016-09-01 22:57:19 +0300

    Remove default acceptable email domain.
    Expect all acceptable email domains to be specified as a comma
    separated list in the env variables.

--- server/middleware/requireEmailWithDomains.js
+++ server/middleware/requireEmailWithDomains.js
@@ -11,20 +11,8 @@ module.exports = (req, res, next, {
    * @param acceptableDomains
    *
    * List of fully qualified accceptable domain names.
-   * Should be for example 'safaricom.co.ke', not just 'safaricom' or 'co.ke'
-   *
-   * IMPORTANT!!
-   * If you use this middleware and add other acceptableDomains, make sure to
-   * add 'safaricom.co.ke' in your list as well. It's added here as a default
-   * and will be overridden if other values are passed.
-   * It's HIGHLY recommended that you don't edit the code to add domains
-   * into the list, rather, add them into your environment as a comma
-   * separated list in an env variable named ACCEPTABLE_DOMAINS.
-   *
-   * Not going to validate that you didn't override 'safaricom.co.ke', I trust
-   * that you're a developer and you what you're doing. ¯\_(ツ)_/¯
    */
-  acceptableDomains = ['safaricom.co.ke']
+  acceptableDomains = []
 } = {}) => {
   let domains = acceptableDomains;
 
