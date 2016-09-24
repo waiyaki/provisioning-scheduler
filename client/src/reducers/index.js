18b8fc6a61d296816fc41cef8edca7ef6899c30e
James Muturi <james.muturi@andela.com>
2016-09-24 11:22:38 +0300

    Remove client application

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-const initialState = { name: 'Provisioning Scheduler' };
-export default function (state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
--- client/src/store/index.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV !== 'production') {
-  /* eslint-disable global-require */
-  module.exports = require('./configureStore.dev');
-} else {
-  module.exports = require('./configureStore.prod');
-  /* eslint-enable global-require */
-}
